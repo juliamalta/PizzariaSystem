449be77d73e201762a12e54818b8355e30bf21cd
juliamalta <juliamalta12@gmail.com>
2023-05-27 18:06:15 -0300

    change tela new

--- src/assets/Pages/Login/Login.jsx
+++ src/assets/Pages/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({onSubmit}) => {
     }
     
 return <form onSubmit={saveSubmit} className="flex flex-col items-center py-40 bg-[url('/img/malta.jpg')] rounded-none mt-20 border-solid border-2 border-gray-500 bg-no-repeat bg-right  ">
-<input  required className=" p-2 w-96  -ml-5 border-solid border-2 border-gray-200 mt-2" type="text" placeholder="Digite o Email"/>
+<input  required className=" p-2 w-96  -ml-5 border-solid border-2 border-gray-200 mt-2" type="text" placeholder="Digite o Usuario"/>
 <input required className="p-2  w-96  -ml-5 border-solid border-2 border-gray-200 mt-2"type="password" placeholder="Digite sua senha"/>
 
 <button type="submit" className="bg-green-200 rounded-full p-4 w-40 mt-10">Entrar</button>
